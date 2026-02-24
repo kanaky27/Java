3d5672fcde00d39ef21ad537346b9aa6cfd6545c
kanak <kanakyadav2764@gmail.com>
2026-02-25 00:01:54 +0530

    First-java

---
+++ src/First_Java/Calculator.java
@@ -0,0 +1,44 @@
+package First_Java;
+
+import java.util.*;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        double result = 0;
+
+        if (operator == '+') {
+            result = num1 + num2;
+        }
+        else if (operator == '-') {
+            result = num1 - num2;
+        }
+        else if (operator == '*') {
+            result = num1 * num2;
+        }
+        else if (operator == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                System.out.println("Error: Division by zero is not allowed.");
+                return;
+            }
+        }
+        else {
+            System.out.println("Invalid operator.");
+            return;
+        }
+
+        System.out.println("Result: " + result);
+
+    }
+}
